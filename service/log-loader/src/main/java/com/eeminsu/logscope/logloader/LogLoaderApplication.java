e07b13bfd2ba4a162982ac63dc9036989932bc81
eeminsu <alstn9623@naver.com>
2025-05-17 16:38:47 +0900

    feat: log-loader 추가

---
+++ service/log-loader/src/main/java/com/eeminsu/logscope/logloader/LogLoaderApplication.java
@@ -0,0 +1,12 @@
+package com.eeminsu.logscope.logloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogLoaderApplication.class, args);
+    }
+}
